ebc05943ed6617813a6067021b00c39a22574a00
AGUDO ALONSO Alio <alioagudo@gmail.com>
2021-01-14 03:38:14 +0100

    feature:  ErrorPage created

---
+++ src/views/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './ErrorPage.scss';
+
+export function ErrorPage() {
+
+
+  return (
+    <React.Fragment>
+      <div className="error-view">
+        <h1>404</h1>
+      </div>
+    </React.Fragment>
+  )
+
+}
