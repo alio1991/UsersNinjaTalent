0fd7b33d1fe6931e8e631206c38fcfb7115c5f5c
AGUDO ALONSO Alio <alioagudo@gmail.com>
2021-01-14 03:38:01 +0100

    feature:  userService created

---
+++ src/services/userService.js
@@ -0,0 +1,60 @@
+const getUsers = [
+        {
+          "id": 1,
+          "firstname": "John",
+          "lastname": "Smith",
+          "email": "john.smith@example.com",
+          "birthDate": "1980-01-23",
+          "address": {
+            "id": 1,
+            "street": "Lindenstraße 89",
+            "city": "Freiburg im Breisgau",
+            "country": "DE",
+            "postalcode": "42030"
+          }
+        },
+        {
+            "id": 2,
+            "firstname": "Pedro",
+            "lastname": "Sanchez",
+            "email": "pedro.sanchez@example.com",
+            "birthDate": "1980-01-23",
+            "address": {
+              "id": 1,
+              "street": "Calle 1",
+              "city": "París",
+              "country": "FR",
+              "postalcode": "67456"
+            }
+          },
+          {
+            "id": 3,
+            "firstname": "David",
+            "lastname": "Velasco",
+            "email": "david.velasco@example.com",
+            "birthDate": "1980-01-23",
+            "address": {
+              "id": 1,
+              "street": "Calle 2",
+              "city": "Roma",
+              "country": "IT",
+              "postalcode": "02030"
+            }
+          },
+          {
+            "id": 4,
+            "firstname": "Juan",
+            "lastname": "Pérez",
+            "email": "juan.perez@example.com",
+            "birthDate": "1980-01-23",
+            "address": {
+              "id": 1,
+              "street": "Calle 3",
+              "city": "Madrid",
+              "country": "ES",
+              "postalcode": "28065"
+            }
+          }
+];
+
+export {getUsers};
