38524982e4d73a18d91c84231f4111ffdc5816d3
AGUDO ALONSO Alio <alioagudo@gmail.com>
2021-01-14 03:37:30 +0100

    feature: router, dependencies and app.js ready

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+import { LandingView } from './views/LandingView/LandingView.jsx';
+import { ErrorPage } from './views/ErrorPage/ErrorPage.jsx';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <HashRouter>
+            <Switch>
+              <Route exact path="/" render={() => <LandingView/>}/> 
+              <Route path="/" render={() => <ErrorPage/>}/>
+              {/* <PrivateRoute component={Periods} path="/configuration/periods" sectionName="configurationHolidayPeriod" /> */}
+            </Switch>
+          </HashRouter>
     </div>
   );
 }
